d6caae286bc2307db91720eb0600a492f45699f4
Justin Scott <hello@jrs.dev>
2025-03-03 07:44:08 -0800

    Create Collectible.cs

---
+++ Collectible.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    [SerializeField] private int _collectibleValue = 1;
+    
+    private UIManager _uiManager;
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        //_uiManager = FindObjectOfType<UIManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _animator.SetTrigger("hasBeenCollected");
+            StartCoroutine(CreateCollectibleFX());
+        }
+    }
+    
+    IEnumerator CreateCollectibleFX()
+    {
+        yield return new WaitForSeconds(0.3f);
+        //_uiManager.SetDiamondCount(_collectibleValue);
+        gameObject.SetActive(false);
+    }
+}
