18512ce1a209bf1ad5956c72d966a1fc49e36a37
Justin Scott <hello@jrs.dev>
2025-03-03 11:58:29 -0800

    Update Collectible.cs

--- Collectible.cs
+++ Collectible.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectibles : MonoBehaviour
+public class Collectible : MonoBehaviour
 {
     [SerializeField] private int _collectibleValue = 1;
     
