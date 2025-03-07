b5bc4dd4208278affa36bae9c21d9ba56f8ac6f4
Justin Scott <hello@jrs.dev>
2025-03-07 08:34:15 -0800

    Update PlayerAnimations.cs

--- PlayerAnimations.cs
+++ PlayerAnimations.cs
@@ -1,13 +1,10 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
     private Rigidbody2D _playerRb;
     private Animator _animator;
-
     private void Awake()
     {
         _playerRb = GetComponentInParent<Rigidbody2D>();
@@ -18,5 +15,19 @@ public class PlayerAnimations : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         _playerRb.bodyType = RigidbodyType2D.Dynamic;
+    }
+    
+    private IEnumerator DeactivatePlayerBody()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(0.3f);
+        GameObject.Find("Level Manager").GetComponent<LevelManager>().LoadNextLevel();
+        
+    }
+
+    public void ExitAnimation()
+    {
+        _playerRb.bodyType = RigidbodyType2D.Static;
+        _animator.SetTrigger("playerExit");
     }
 }
