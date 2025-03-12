25fc51a0f100e03995280efb7f2f4bc58d31efbc
Justin Scott <hello@jrs.dev>
2025-03-12 14:01:50 -0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] int damage = 3;
+
+    private PlayerHealth _playerHealth;
+
+    private void Start()
+    {
+        _playerHealth = FindObjectOfType<PlayerHealth>();
+    }
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<Player>().Knockback();
+            _playerHealth.TakeDamage(damage);
         }
     }
 }
