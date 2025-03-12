1e20e0d8aca1e5920ba88e5150d241f41bf8bbfa
Justin Scott <hello@jrs.dev>
2025-03-12 07:43:03 -0700

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Player>().Knockback();
+        }
+    }
+}
